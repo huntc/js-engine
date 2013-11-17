688cab8d6e61926d998c1f1a1bdfbb860cded70b
Christopher Hunt <huntchr@gmail.com>
2013-11-17 11:02:17 +1100

    Factored out Rhino shell concerns into its own actor thus avoiding the potential for deadlocks given stream overflows i.e. IO actors and the shell actors are all executing separately.

--- src/main/scala/com/typesafe/jse/Rhino.scala
+++ src/main/scala/com/typesafe/jse/Rhino.scala
@@ -1,104 +1,67 @@
 package com.typesafe.jse
 
-import com.typesafe.jse.Engine.ExecuteJs
-import akka.actor.{Terminated, Props}
-import org.mozilla.javascript.tools.shell.{Global, Main}
+import akka.actor.{ActorRef, Actor, Props}
+import org.mozilla.javascript.tools.shell.Main
 import scala.collection.mutable.ListBuffer
 import org.mozilla.javascript._
 import scala.concurrent.blocking
-import java.io.{OutputStream, PipedInputStream, PipedOutputStream}
+import java.io._
 import akka.contrib.process.StreamEvents.Ack
 import akka.contrib.process.Source
+import scala.collection.immutable
 import com.typesafe.jse.Engine.ExecuteJs
+import akka.actor.Terminated
 
 /**
  * Declares an in-JVM Rhino based JavaScript engine. The actor is expected to be
- * associated with a blocking dispatcher as calls to Rhino are blocking.
+ * associated with a blocking dispatcher as calls to Rhino and its use of Jdk streams
+ * are blocking.
  */
 class Rhino extends Engine {
 
-  def withContext(block: => Unit): Unit = {
-    Context.enter()
-    try block finally {
-      Context.exit()
-    }
-  }
+  // The main objective of this actor implementation is to establish actors for both the execution of
+  // Rhino code (Rhino's execution is blocking), and actors for the source of stdio (which is also blocking).
+  // This actor is then a conduit of the IO as a result of execution.
+
+  val stdoutOs = new PipedOutputStream()
+  val stderrOs = new PipedOutputStream()
+
+  val stdoutIs = new PipedInputStream(stdoutOs)
+  val stderrIs = new PipedInputStream(stderrOs)
 
   expectOnce {
     case ExecuteJs(source, args, timeout, timeoutExitValue) =>
       val requester = sender
 
-      val stdoutOs = new PipedOutputStream()
-      val stderrOs = new PipedOutputStream()
+      val stdoutSource = context.actorOf(Source.props(stdoutIs, self))
+      val stderrSource = context.actorOf(Source.props(stderrIs, self))
 
-      try {
-        val stdoutIs = new PipedInputStream(stdoutOs)
-        val stderrIs = new PipedInputStream(stderrOs)
+      new EngineIOHandler(stdoutSource, stderrSource, requester, Ack, timeout, timeoutExitValue)
 
-        // The sources are responsible for closing the input streams.
-        val stdoutSource = context.watch(context.actorOf(Source.props(stdoutIs, self)))
-        val stderrSource = context.watch(context.actorOf(Source.props(stderrIs, self)))
+      context.actorOf(RhinoShell.props(
+        source.getParentFile.getCanonicalFile,
+        immutable.Seq(source.getCanonicalPath) ++ args,
+        stdoutOs, stdoutSource,
+        stderrOs, stderrSource
+      ))
+  }
 
-        new EngineIOHandler(stdoutSource, stderrSource, requester, Ack, timeout, timeoutExitValue)
+  def closeSafely(closable: Closeable): Unit = {
+    try {
+      closable.close()
+    } catch {
+      case _: Exception =>
+    }
+  }
 
-        // Initialise our Rhino environment. If we've never done so before then do general Rhino shell
-        // initialisation and then override its print function. The Rhino shell is all static so this
-        // only need be done once.
-        if (!Main.getGlobal.isInitialized) {
-          Main.getGlobal.init(Main.shellContextFactory)
-          withContext {
-            Main.getGlobal.defineFunctionProperties(Array("print"), classOf[Rhino], ScriptableObject.DONTENUM)
-          }
-        }
-
-        // Each time we use Rhino we set properties in the global scope that represent the stdout and stderr
-        // output streams. These output streams are plugged into our source actors.
-        withContext {
-          def jsStdoutOs = Context.javaToJS(stdoutOs, Main.getGlobal)
-          ScriptableObject.putProperty(Main.getGlobal, "stdout", jsStdoutOs)
-          def jsStderrOs = Context.javaToJS(stderrOs, Main.getGlobal)
-          ScriptableObject.putProperty(Main.getGlobal, "stderr", jsStderrOs)
-        }
-
-        // Formulate arguments to the Rhino shell.
-        val lb = ListBuffer[String]()
-        lb ++= Seq(
-          "-opt", "-1",
-          "-modules", source.getParentFile.getCanonicalPath,
-          source.getCanonicalPath
-        )
-        lb ++= args
-
-        // Call the Rhino shell. We have to assume that Rhino is blocking - who knows what will be going on
-        // within the submitted JS?
-        // FIXME: Given that it is blocking, we're not going to be in a position to process source output
-        // until its finished. This could mean that Rhino is blocked from writing and thus result in a
-        // deadlock. The solution is for Rhino to execute in its own thread - perhaps as its own actor?
-        // At this point flushing the print function will become something worthy of consideration - to
-        // flush as we go or not?
-        val exitCode = blocking {
-          Main.exec(lb.toArray)
-        }
-
-        // When all streams are closed then we signal that Rhino has exited. This is entirely satisfactory in
-        // a Rhino shell situation given that we are in control of the stdout and stderr. Our contract to the
-        // outside world (EngineIOHandler) is that the exit status is always sent after stdout and stderr as
-        // per the akka.contrib.process package.
-        var openStreams = 2
-        expect {
-          case Terminated(`stdoutSource` | `stderrSource`) =>
-            openStreams -= 1
-            if (openStreams == 0) {
-              self ! exitCode
-            }
-        }
-
-      } finally {
-        blocking {
-          stdoutOs.close()
-          stderrOs.close()
-        }
-      }
+  override def postStop() = {
+    // Be paranoid and ensure that all resources are cleared up.
+    blocking {
+      closeSafely(stderrIs)
+      closeSafely(stdoutIs)
+      closeSafely(stderrOs)
+      closeSafely(stdoutOs)
+    }
   }
 
 }
@@ -111,13 +74,109 @@ object Rhino {
     Props(classOf[Rhino])
   }
 
+}
+
+
+/**
+ * Manage the execution of the Rhino shell setting up its environment, running the main entry point
+ * and sending its parent the exit code when it can see that the stdio sources have closed.
+ */
+private[jse] class RhinoShell(
+                               moduleBase: File,
+                               args: immutable.Seq[String],
+                               stdoutOs: OutputStream, stdoutSource: ActorRef,
+                               stderrOs: OutputStream, stderrSource: ActorRef
+                               ) extends Actor {
+
+  // A utility to safely manage Rhino contexts.
+  def withContext(block: => Unit): Unit = {
+    Context.enter()
+    try block finally {
+      Context.exit()
+    }
+  }
+
+  // Initialise our Rhino environment. If we've never done so before then do general Rhino shell
+  // initialisation and then override its print function. The Rhino shell is all static so this
+  // only need be done once.
+  if (!Main.getGlobal.isInitialized) {
+    Main.getGlobal.init(Main.shellContextFactory)
+    withContext {
+      Main.getGlobal.defineFunctionProperties(Array("print"), classOf[RhinoShell], ScriptableObject.DONTENUM)
+    }
+  }
+
+  // Each time we use Rhino we set properties in the global scope that represent the stdout and stderr
+  // output streams. These output streams are plugged into our source actors.
+  withContext {
+    def jsStdoutOs = Context.javaToJS(stdoutOs, Main.getGlobal)
+    ScriptableObject.putProperty(Main.getGlobal, "stdout", jsStdoutOs)
+    def jsStderrOs = Context.javaToJS(stderrOs, Main.getGlobal)
+    ScriptableObject.putProperty(Main.getGlobal, "stderr", jsStderrOs)
+  }
+
+  // Formulate arguments to the Rhino shell.
+  val lb = ListBuffer[String]()
+  lb ++= Seq(
+    "-opt", "-1",
+    "-modules", moduleBase.getCanonicalPath
+  )
+  lb ++= args
+
+
+  val exitCode = blocking {
+    try {
+      Main.exec(args.toArray)
+    } finally {
+      stdoutOs.close()
+      stderrOs.close()
+    }
+  }
+
+  // When all streams are closed then we signal that Rhino has exited. This is entirely satisfactory in
+  // a Rhino shell situation given that we are in control of the stdout and stderr. Our contract to the
+  // outside world (EngineIOHandler) is that the exit status is always sent after stdout and stderr as
+  // per the akka.contrib.process package.
+
+  context.watch(stdoutSource)
+  context.watch(stderrSource)
+
+  var openStreams = 2
+
+  def receive = {
+    case Terminated(`stdoutSource` | `stderrSource`) =>
+      openStreams -= 1
+      if (openStreams == 0) {
+        context.parent ! exitCode
+      }
+  }
+
+}
+
+private[jse] object RhinoShell {
+  def props(
+             moduleBase: File,
+             args: immutable.Seq[String],
+             stdoutOs: OutputStream, stdoutSource: ActorRef,
+             stderrOs: OutputStream, stderrSource: ActorRef
+             ): Props = {
+    Props(classOf[RhinoShell], moduleBase, args, stdoutOs, stdoutSource, stderrOs, stderrSource)
+  }
+
   private val lineSeparator = System.getProperty("line.separator").getBytes("UTF-8")
 
   /*
    * Our override of Rhino's print function. Output is sent to the stdout source. There is no provision in Rhino's
    * shell to send to stderr.
+   *
+   * Has to be public in order for the Rhino shell to find it.
    */
-  def print(cx: Context, thisObj: Scriptable, args: Array[Any], funObj: org.mozilla.javascript.Function): Any = {
+  def print(
+             cx: Context,
+             thisObj: Scriptable,
+             args: Array[Any],
+             funObj: org.mozilla.javascript.Function
+             ): Any = {
     val property = funObj.getParentScope.get("stdout", thisObj)
     property match {
       case jsOs: NativeJavaObject =>
@@ -132,5 +191,4 @@ object Rhino {
     }
     Context.getUndefinedValue
   }
-
 }
