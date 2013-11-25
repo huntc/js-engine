2874c57b467eb0ccad074749b155c7193fa4af4f
Christopher Hunt <huntchr@gmail.com>
2013-11-25 12:01:10 +1100

    Changed the test to support both Rhino and common-node.

--- js-engine-tester/src/main/resources/com/typesafe/jse/tester/test.js
+++ js-engine-tester/src/main/resources/com/typesafe/jse/tester/test.js
@@ -1,10 +1,9 @@
-//var args = require('system').args;
+var log;
+try {
+    log = require("console").log;
+} catch (e) {
+    log = print;
+}
 
-print("Hi there");
-print("and again!");
- //console.log(args[2]);
- //if (phantom !== undefined) phantom.exit();
-/*importPackage(Packages.com.typesafe.jse);
-engineActor.tell(Engine$OutputEvent$.MODULE$.apply("Hi there"), null);
-engineActor.tell(Engine$FinalEvent$.MODULE$, null);
-                                               */
+log("Hi there");
+log("and again!");
