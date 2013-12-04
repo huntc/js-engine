21f24aa7c9a3f345024024fe57abda40c362b7c0
Christopher Hunt <huntchr@gmail.com>
2013-12-04 18:28:36 +1100

    Now leverages the sbt-web provided ActorRefFactory.

--- sbt-js-engine/src/main/scala/com/typesafe/jse/sbt/JsEnginePlugin.scala
+++ sbt-js-engine/src/main/scala/com/typesafe/jse/sbt/JsEnginePlugin.scala
@@ -1,9 +1,6 @@
 package com.typesafe.jse.sbt
 
 import sbt._
-import akka.actor.ActorSystem
-import akka.util.Timeout
-import scala.concurrent.duration._
 
 /**
  * Declares the main parts of a WebDriver based plugin for sbt.
@@ -22,27 +19,9 @@ object JsEnginePlugin extends sbt.Plugin {
 
   import JsEngineKeys._
 
-  implicit val jseSystem = withActorClassloader(ActorSystem("jse-system"))
-  implicit val jseTimeout = Timeout(5.seconds)
-
   override val globalSettings: Seq[Setting[_]] = Seq(
     engineType := EngineType.Rhino,
     parallelism := java.lang.Runtime.getRuntime.availableProcessors() + 1
   )
 
-  /*
-   * Sometimes the class loader associated with the actor system is required e.g. when loading configuration in sbt.
-   */
-  private def withActorClassloader[A](f: => A): A = {
-    val newLoader = ActorSystem.getClass.getClassLoader
-    val thread = Thread.currentThread
-    val oldLoader = thread.getContextClassLoader
-
-    thread.setContextClassLoader(newLoader)
-    try {
-      f
-    } finally {
-      thread.setContextClassLoader(oldLoader)
-    }
-  }
 }
